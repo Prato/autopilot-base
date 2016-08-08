804291e3cde00b6f04859b8826e07a25dae9847e
Jeffrey Prato <jeffreyprato@gmail.com>
2016-08-08 11:33:05 -0500

    scratch test

---
+++ README-tldr.md
@@ -0,0 +1,3 @@
+# autopilot-base
+
+Autopilot Pattern implementation Alpine base image with Consul and Containerpilot
--- release.sh
+++ release.sh
@@ -6,11 +6,14 @@
       # echo "update.sh version called. "
 
 #     local new_ver = shift
-#     sed -i -e '/^Version.*$/Version ${NEW_VER}/' README.md
+#     sed -i -e 's/^Version.*$/Version ${NEW_VER}/' README.md
+#     sed -i -e 's|^\s+image: .*|${REPO_NAME}:${NEW_VER}|o' docker-compose.yml
 #     git tag $NEW_VER
-#     git push --follow-tags
+#     git push; git push --tags
 # }
 # except auto-gen the ver tag in here.
+
+# or just use make
 # cmd=$1
 # if [ ! -z "$cmd" ]; then
 #     shift 1
